3f5b5195fda4453ce7d7688d69c19b79f7e823b0
Ayhan G.Y <82941858+ayhan-dev@users.noreply.github.com>
2023-08-13 17:00:44 +0330

    Update NODELS.js

--- NODELS.js
+++ NODELS.js
@@ -3,6 +3,19 @@ const request       = require('request');
 
 const token         = '6317234969:AA....';
 const bot           = new TelegramBot(token,{polling:true});
+ 
+function getUserData(chat) {
+  let user = storage[chat]
+  if (!user) {
+    user = {
+      waitingForCity:    false,
+      waitingForWeather: false,
+      waitingForTime:    false,
+    }
+    storage[chat] = user
+  }
+  return user
+}
 
 bot.onText(/\/start/, (msg) => {
   const chatId = msg.chat.id
@@ -18,3 +31,20 @@ bot.onText(/\/start/, (msg) => {
   )
 })
 
+
+
+
+bot.on('callback_query', async (callbackQuery) => {
+  const chatId = callbackQuery.message.chat.id
+  const data   = callbackQuery.data
+
+  switch (data) {
+    case 'ayhan':
+      const userDataWeather = getUserData(chatId)
+      userDataWeather.waitingForCity    = true
+      userDataWeather.waitingForWeather = true
+      bot.sendMessage(chatId, 'my ayhan @Ayhan_Dev')
+      break
+
+  }
+})
