e006d6a7054cfb652bfe717dd9ca287b4626ae4f
Ayhan G.Y <82941858+ayhan-dev@users.noreply.github.com>
2023-08-19 13:30:59 +0330

    Update Bot.py

--- Python/Bot.py
+++ Python/Bot.py
@@ -1,1 +1,10 @@
 
+from pyrogram import Client, filters
+
+app = Client("64646:")
+
+@app.on_message(filters.command("start"))
+def start(client, message):
+    message.reply_text("hi ayhan - @ayhan_Dev")
+
+app.run()
