ce28700d312b35f72d87ef86f4e5554a6b02f26c
Ayhan G.Y <82941858+ayhan-dev@users.noreply.github.com>
2023-08-19 13:47:22 +0330

    Update bot.lua

--- Lua/bot.lua
+++ Lua/bot.lua
@@ -1,7 +1,15 @@
 local telegram = require('telegram-bot-lua')
-local bot = telegram('383838:AA')
+local bot = telegram('383838:AA')--token bot
+
 bot.command('start', function(message)
-    bot.sendMessage(message.chat.id, 'hi Ayhan - @Ayhan_Dev')
+    local keyboard = {
+        {"Click in"}
+    }
+    bot.sendMessage(message.chat.id, "Hi Ayhan - @Ayhan_Dev", nil, false, nil, telegram.inlineKeyboard(keyboard))
 end)
 
-bot.start()
+bot.on('callbackQuery', function(callback_query)
+    if callback_query.data == 'click' then
+        bot.answerCallbackQuery(callback_query.id, "hi :)🐈 ")
+    end
+end)
